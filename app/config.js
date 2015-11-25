0b3209835f653aedb7bfc27c49b99cc01cfe83a0
platypusrex <frankcooke79@gmail.com>
2015-11-24 20:07:45 -0500

    added project structure, require, bower, and initial index.html skeleton with dependencies

---
+++ app/config.js
@@ -0,0 +1,10 @@
+require.config({
+    deps: ['main'],
+
+    paths: {
+        jquery: '../assets/js/jquery/src/jquery',
+        underscore: '../assets/js/underscore-amd/underscore',
+        backbone: '../assets/js/backbone-amd/backbone',
+        tpl: '../assets/js/requirejs-tpl/tpl'
+    }
+});
